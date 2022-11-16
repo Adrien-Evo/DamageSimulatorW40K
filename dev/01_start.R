924777e1344d63dcd5cc8a6996cefc7ded1ff130
AF <adrien.foucal@gmail.com>
2022-11-16 16:06:41 +0100

    Adding a lot of files, lots of stuff from golem that Idon't understand well yet

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,12 +20,16 @@
 ##
 golem::fill_desc(
   pkg_name = "DamageSimulatorW40K", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL # The URL of the GitHub Repo (optional)
+  pkg_title = "A ShinyApp to know what to expect when you shoot at things in W40K", # The Title of the package containing the App
+  pkg_description = "In this app, you can easily compute the damage output of any weapons against any targets.
+  Just use the sliders to adjust the parameters and it will produce data to help guide your decisions.
+  It uses a binomial distribution with a probability computed with the parameters given.
+  You'll be served graphs (probability density of the binomial for those of you stats enclined)
+  giving you the probability to wound a certain amount of damages with some usefull extra stats displayed.", # The Description of the package containing the App
+  author_first_name = "Adrien", # Your First Name
+  author_last_name = "Foucal", # Your Last Name
+  author_email = "adrien.foucal@gmail.com", # Your Email
+  repo_url = "https://github.com/Adrien-Evo/DamageSimulatorW40K.git" # The URL of the GitHub Repo (optional)
 )
 
 ## Set {golem} options ----
@@ -50,10 +54,10 @@ golem::use_recommended_tests()
 
 ## Favicon ----
 # If you want to change the favicon (default is golem's one)
-golem::use_favicon() # path = "path/to/ico". Can be an online file.
+golem::use_favicon("C:\\Users\\Adrien\\Downloads\\target.png")
 # golem::remove_favicon() # Uncomment to remove the default favicon
-
-## Add helper functions ----
+#Vitaly Gorbachev
+  ## Add helper functions ----
 golem::use_utils_ui(with_test = TRUE)
 golem::use_utils_server(with_test = TRUE)
 
