f07264a4c816f47c9275a436a5c6a96268b23191
AF <adrien.foucal@gmail.com>
2022-12-22 16:35:20 +0100

    Adding Roxygen2 documentation

--- R/stats_calculator.R
+++ R/stats_calculator.R
@@ -15,11 +15,17 @@ library(shinythemes)
 #necron <- xmlParse("W40K/Necrons.cat")
 #xml_necron <- xmlToList(necron)
 
-##Getting a graph simply
-#We assume EQM : equivalent marines saves 3+ toughness 4
-#Probability = 20/216 3+ =1-  3-1/6 * 1-3/6 *5/6 ==wound
-####TOughness = 4 for the moment
 
+
+
+#' Get the roll needed to wound
+#'
+#' @param ST Weapon strength.
+#' @param TO Toughness of the target.
+#' @return Needed wound roll
+#' @examples
+#' get_strength_toughness_prob(4, 4)
+#' get_strength_toughness_prob(10,5)
 get_strength_toughness_prob <- function(ST, TO){
   prob = 0
   if(2*ST <= TO){
@@ -43,26 +49,57 @@ get_binom_prob <- function(hit,strength,toughness,armor){
   prob = prob * ((armor-1)/6)
   return(prob)
 }
-
+#' Get the probability needed to hit
+#'
+#' @param hit Ballistic skill or Weapon skill. It's a roll needed
+#' @return Probability to hit
+#' @examples
+#' get_prob_hit(5)
+#' get_prob_hit(2)
 get_prob_hit <- function(hit){
   prob = 1-(hit-1)/6
   return(prob)
 }
 
+#' Get the probability needed to wound
+#'
+#' @param strength Weapon strength.
+#' @param toughness Toughness of the target.
+#' @return Probability to wound
+#' @examples
+#' get_prob_wound(4, 4)
+#' get_prob_wound(4, 8)
 get_prob_wound <- function(strength,toughness){
   prob = 1-((get_strength_toughness_prob(strength,toughness)-1)/6)
   return(prob)
 }
 
+#' Get the probability for a wound to go througth knowing the save of the opponent
+#'
+#' @param armorsave Armor save in the datasheet
+#' @return Probability for a wound to go through knowing the target armor save
+#' @examples
+#' get_prob_save(4)
+#' get_prob_save(2)
 get_prob_save <- function(armorsave){
   prob = (armorsave-1)/6
   return(prob)
 }
 
+#' Get the number of dead units knowing how many wounds have been dealt, with how many damage per shots and the target's wound characteristic
+#'
+#' @param nbwounds Total number of wounds dealt after hitting, wounding and armorsaving
+#' @param damage Damage profile of the weapon
+#' @param lifepoints Total wounds of the model
+#' @return Number of dead model
+#' @examples
+#' get_prob_save(4)
+#' get_prob_save(2)
 get_kill_count <- function(nbwounds, damage, lifepoints){
   bodycount = nbwounds/(ceiling(lifepoints/damage))
   return(floor(bodycount))
 }
+
 #hit = 4
 #wound = 4
 #armor = 4
