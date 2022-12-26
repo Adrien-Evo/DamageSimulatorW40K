13c0863f5ab8579ba3cbb24fdaad239769ff706b
AF <adrien.foucal@gmail.com>
2022-12-26 14:22:48 +0100

    Refactoring of the app server and app UI

--- R/app_server.R
+++ R/app_server.R
@@ -32,5 +32,32 @@ get_binom_prob <- function(hit,strength,toughness,armor){
 
 
 app_server <- function(input, output, session) {
-  # Your application server logic
+  ##Lets get the probability to get a damage through
+
+  output$probDens <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    hits <- input$NumberHits
+    prob_hit <- get_prob_hit(input$WS)
+    prob_wound <- get_prob_wound(input$Strength,input$TargetToughness)
+    prob_go_through_save <- get_prob_save(input$TargetSave)
+    #Combine all prob using get_prob_save
+    prob_binom <- get_binom_prob(prob_hit,prob_wound,prob_go_through_save)
+    # draw the histogram with the specified number of bins
+    plot(seq(1,hits),dbinom(seq(1,hits),hits,prob=prob_binom),type='h')
+
+  })
+  output$probDens_old <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    hits <- input$NumberHits
+    ##Hit modifyer
+    if(input$noArmor){
+      armor = 7
+    }else{
+      armor = input$TargetSave
+    }
+    pp = get_binom_prob_old(input$WS,input$Strength,input$TargetToughness,armor)
+    # draw the histogram with the specified number of bins
+    plot(seq(1,hits),dbinom(seq(1,hits),hits,prob=pp),type='h')
+
+  })
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -17,39 +17,46 @@ app_ui <- function(request) {
     golem_add_external_resources(),
     # Your application UI logic
     fluidPage(
-      h1("DamageSimulatorW40K"),
-      theme = shinytheme("darkly"),
-      sidebarPanel(
+      theme = shinytheme("united"),
+      titlePanel("DamageTesting"),
+      fluidRow(
+        column(5,
+               "DamageDealer",
+               # Copy the line below to make a slider bar
+               sliderInput("NumberHits", label = h3("Number of hits"), min = 1,
+                           max = 60, value = 10),
 
-        # Copy the line below to make a slider bar
-        sliderInput("NumberHits", label = h3("Number of hits"), min = 1,
-                    max = 60, value = 10),
-
-        # Copy the line below to make a slider bar
-        sliderInput("WS", label = h3("Ballistic or weapon skill "), min = 1,
-                    max = 6, value = 3),
-
-
-        # Copy the line below to make a slider range
-        sliderInput("Strength", label = h3("Strength"), min = 1,
-                    max = 12 , value = 4),
-
-        # Copy the line below to make a slider range
-        sliderInput("TargetToughness", label = h3("TargetToughness"), min = 1,
-                    max = 12, value = 4),
-
-        # Copy the line below to make a slider range
-        sliderInput("TargetSave", label = h3("TargetSave"), min = 1,
-                    max = 6, value = 3),
-        checkboxInput("noArmor", "No Armor Save", value = FALSE)
-      ),
+               # Copy the line below to make a slider bar
+               sliderInput("WS", label = h3("Ballistic or weapon skill "), min = 1,
+                           max = 6, value = 3),
 
 
-      mainPanel(
+               # Copy the line below to make a slider range
+               sliderInput("Strength", label = h3("Strength"), min = 1,
+                           max = 12 , value = 4),
+               sliderInput("Damage", label = h3("Damage"), min = 1,
+                           max = 12 , value = 4)
+        ),
+        column(8,
+               "main",
+               tabsetPanel(type = "tabs",
+                           tabPanel("ProbDens",plotOutput(outputId = "probDens")),
+                           tabPanel("ProbDens_old", plotOutput(outputId ="probDens_old"))
+               )
+        ),
+        column(5,
+               "OppositionDefense",
+               # Copy the line below to make a slider range
+               sliderInput("TargetToughness", label = h3("TargetToughness"), min = 1,
+                           max = 12, value = 4),
 
-        # Output: Histogram ----
-        plotOutput(outputId = "probDens")
-
+               # Copy the line below to make a slider range
+               sliderInput("TargetSave", label = h3("TargetSave"), min = 1,
+                           max = 6, value = 3),
+               checkboxInput("noArmor", "No Armor Save", value = FALSE),
+               sliderInput("TargetLifePoint", label = h3("TargetLifePoints"), min = 1,
+                           max = 10, value = 2)
+        )
       )
     )
   )
