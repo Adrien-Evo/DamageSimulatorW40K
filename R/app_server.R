ce8cee3fccc550d7c36e6844d8261c837f46af52
AF <adrien.foucal@gmail.com>
2022-12-26 16:32:39 +0100

    Making plots look much better
    Using now the barplot function with added text to get the % for each barplot. Cex is nice at 1 even if it could be dinamically adjusted for the number of bars. But if I want to remove some of the bar that are close from 0, then a fixed value should be good enough.

--- R/app_server.R
+++ R/app_server.R
@@ -15,10 +15,16 @@ app_server <- function(input, output, session) {
     prob_hit <- get_prob_hit(input$WS)
     prob_wound <- get_prob_wound(input$Strength,input$TargetToughness)
     prob_go_through_save <- get_prob_save(input$TargetSave)
+
     #Combine all prob using get_prob_save
     prob_binom <- get_binom_prob(prob_hit,prob_wound,prob_go_through_save)
+
+    # Get the prob to damage a model
+    binom_damage <- dbinom(seq(1,hits),hits,prob=prob_binom)
+
     # draw the histogram with the specified number of bins
-    plot(seq(1,hits),dbinom(seq(1,hits),hits,prob=prob_binom),type='h')
+    damage_barplot <- barplot(binom_damage,names.arg = seq(1,hits),col="#69b3a2",ylim = c(0,max(binom_damage)+0.05))
+    text(damage_barplot, binom_damage + 0.025 , paste(round(binom_damage*100),"%", sep=""),cex=1)
 
   })
   output$kill_dens <- renderPlot({
@@ -30,9 +36,14 @@ app_server <- function(input, output, session) {
     prob_go_through_save <- get_prob_save(input$TargetSave)
     #Combine all prob using get_prob_save
     prob_binom <- get_binom_prob(prob_hit,prob_wound,prob_go_through_save)
-    # draw the histogram with the specified number of bins
+    # get the initial kill count from which the dbinoml will be computed
     nb_kills <- get_kill_count(hits, input$Damage, input$TargetLifePoints)
-    plot(seq(1,nb_kills),dbinom(seq(1,nb_kills),nb_kills,prob=prob_binom),type='h')
 
+    #Get the prob to kill a model
+    binom_kill <- dbinom(seq(1,nb_kills),nb_kills,prob=prob_binom)
+
+    # draw the histogram with the specified number of bins
+    kill_barplot <- barplot(binom_kill,names.arg = seq(1,nb_kills),col="#69b3a2",ylim = c(0,max(binom_kill)+0.05))
+    text(kill_barplot, binom_kill + 0.025 , paste(round(binom_kill*100),"%", sep=""),cex=1)
   })
 }
